42c127c1821705fe7f9eacf7560125ef9f9f38be
Rohit Govindrao Bachche <129142269+silentrohit@users.noreply.github.com>
2023-03-28 10:51:35 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+class  add
+{
+	public static void main(String[] args) 
+	{
+		Scanner x=new Scanner(System.In)
+		System.out.println("Enter the first number!");
+		int num1 =x.next Int ();
+		System.out.println("Enter the second number!");
+		int num2 =x.next Int();
+		int result=num1+num2;
+		System.out.println("The sum of "+num1+""and""+num2+"is!"+result);
+
+	}
+}
